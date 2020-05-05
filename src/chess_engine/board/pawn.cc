dc4698c0dd1e51b68fda70cb5c3aeaae922bf32f
Guillaume Merle <guillaume.merle@epita.fr>
2020-05-05 23:36:17 +0200

    [BOARD] pawn movements and generation

--- src/chess_engine/board/pawn.cc
+++ src/chess_engine/board/pawn.cc
@@ -4,7 +4,7 @@ namespace board
 {
     Bitboard Pawn::single_push(Bitboard bitboard, Color color)
     {
-        if (color == Color.WHITE)
+        if (color == Color::WHITE)
             return north(bitboard, 1);
 
         return south(bitboard, 1);
@@ -12,7 +12,7 @@ namespace board
 
     Bitboard Pawn::double_push(Bitboard bitboard, Color color)
     {
-        if (color == Color.WHITE)
+        if (color == Color::WHITE)
             return north(bitboard, 2);
 
         return south(bitboard, 2);
@@ -20,7 +20,7 @@ namespace board
 
     Bitboard Pawn::left_attack(Bitboard bitboard, Color color)
     {
-        if (color == Color.WHITE)
+        if (color == Color::WHITE)
             return northwest(bitboard, 1);
 
         return southeast(bitboard, 1);
@@ -28,9 +28,34 @@ namespace board
 
     Bitboard Pawn::right_attack(Bitboard bitboard, Color color)
     {
-        if (color == Color.WHITE)
+        if (color == Color::WHITE)
             return northeast(bitboard, 1);
 
         return southwest(bitboard, 1);
+    }
+
+    std::vector<Move>
+    Pawn::generate_moves(std::vector<Move> moves, Bitboard bitboard,
+                         Color color)
+    {
+        auto generated = Pawn::single_push(bitboard, color);
+
+        if (generated)
+            moves.emplace_back(Move(bitboard, generated, PieceType::PAWN));
+
+        if ((color == Color::WHITE && bitboard <= white_pawn_start)
+                || (color == Color::BLACK && bitboard >= black_pawn_start))
+        {
+            moves.emplace_back(
+                    Move(bitboard, Pawn::double_push(bitboard, color),
+                         PieceType::PAWN));
+        }
+
+        // moves.emplace_back(Move(bitboard, Pawn::left_attack(bitboard, color),
+                                // PieceType.PAWN));
+        // moves.emplace_back(Move(bitboard, Pawn::right_attack(bitboard, color),
+                                // PieceType.PAWN));
+
+        return moves;
     }
 }
--- src/chess_engine/board/pawn.hh
+++ src/chess_engine/board/pawn.hh
@@ -1,15 +1,25 @@
 #pragma once
 
+#include <vector>
+
 #include "bitboard.hh"
 #include "color.hh"
+#include "move.hh"
 
 namespace board
 {
+    const Bitboard white_pawn_start = 1 << 15;
+    const Bitboard black_pawn_start = 1ULL << 55;
+
     class Pawn
     {
+    public:
         static Bitboard single_push(Bitboard bitboard, Color color);
         static Bitboard double_push(Bitboard bitboard, Color color);
         static Bitboard left_attack(Bitboard bitboard, Color color);
         static Bitboard right_attack(Bitboard bitboard, Color color);
+
+        static std::vector<Move>
+        generate_moves(std::vector<Move> moves, Bitboard bitboard, Color color);
     };
 }
