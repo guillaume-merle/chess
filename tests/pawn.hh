219bb729ae126caf516fe811ca64448037543a78
Valentin Grussenmeyer <valentin.grussenmeyer@epita.fr>
2020-05-06 23:23:08 +0200

    [TESTS] first gtests implementation

---
+++ tests/pawn.hh
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+TEST (Dummy_Back, DummyTest)
+{
+    EXPECT_EQ(0, 0);
+}
+
+TEST (Dummy, DummyTest)
+{
+    EXPECT_EQ(0, 0);
+}
+
+TEST (Dummy, DummyTest2)
+{
+    EXPECT_EQ(0, 0);
+}
