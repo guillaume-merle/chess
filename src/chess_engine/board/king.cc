3f172cc5e8e7fde0e839130fe740fcd00df48d5a
Marc Demoustier <marc.demoustier@epita.fr>
2020-05-06 22:36:36 +0400

    add king moves generation

---
+++ src/chess_engine/board/king.cc
@@ -0,0 +1,50 @@
+#include "king.hh"
+
+namespace board
+{
+    std::vector<Move>
+    King::generate_moves(std::vector<Move>& moves, Bitboard bitboard)
+    {
+        Bitboard generated = north(bitboard, 1);
+        if (generated)
+            moves.emplace_back(Move(bitboard, generated, PieceType::KING));
+
+        generated = south(bitboard, 1);
+        if (generated)
+            moves.emplace_back(Move(bitboard, generated, PieceType::KING));
+
+        // the king is not on the left side
+        if (left_side & bitboard != 0)
+        {
+            generated = west(bitboard, 1);
+            if (generated)
+                moves.emplace_back(Move(bitboard, generated, PieceType::KING));
+
+            generated = northwest(bitboard, 1);
+            if (generated)
+                moves.emplace_back(Move(bitboard, generated, PieceType::KING));
+
+            generated = southwest(bitboard, 1);
+            if (generated)
+                moves.emplace_back(Move(bitboard, generated, PieceType::KING));
+        }
+
+        // the king is not on the right side
+        if (right_side & bitboard != 0)
+        {
+            generated = east(bitboard, 1);
+            if (generated)
+                moves.emplace_back(Move(bitboard, generated, PieceType::KING));
+
+            generated = northeast(bitboard, 1);
+            if (generated)
+                moves.emplace_back(Move(bitboard, generated, PieceType::KING));
+
+            generated = southeast(bitboard, 1);
+            if (generated)
+                moves.emplace_back(Move(bitboard, generated, PieceType::KING));
+        }
+
+        return moves;
+    }
+}
