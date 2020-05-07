2c83f2ba12e3db92a3f9c5f6f680d125a6aa3531
Guillaume Merle <guillaume.merle@epita.fr>
2020-05-07 19:09:42 +0200

    [BOARD] add single_push and double_push tests

--- tests/test_pawn.hh
+++ tests/test_pawn.hh
@@ -3,10 +3,34 @@
 #include "pawn.hh"
 #include "bitboard.hh"
 
-TEST (Pawn, single_move)
+TEST (Pawn, single_push_white)
 {
     board::Bitboard pawn = 1 << 8;
     board::Bitboard move = board::Pawn::single_push(pawn, board::Color::WHITE);
 
     EXPECT_EQ(1 << 16, move);
+}
+
+TEST (Pawn, single_push_black)
+{
+    board::Bitboard pawn = 1ULL << 48;
+    board::Bitboard move = board::Pawn::single_push(pawn, board::Color::BLACK);
+
+    EXPECT_EQ(1ULL << 40, move);
+}
+
+TEST (Pawn, double_push_white)
+{
+    board::Bitboard pawn = 1 << 8;
+    board::Bitboard move = board::Pawn::double_push(pawn, board::Color::WHITE);
+
+    EXPECT_EQ(1ULL << 24, move);
+}
+
+TEST (Pawn, double_push_black)
+{
+    board::Bitboard pawn = 1ULL << 48;
+    board::Bitboard move = board::Pawn::double_push(pawn, board::Color::BLACK);
+
+    EXPECT_EQ(1ULL << 32, move);
 }
