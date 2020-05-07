05019f1b306ea3189745ea65f6d5af5edeab0272
Alexandre DEISS <alexandre.deiss@epita.fr>
2020-05-08 00:57:36 +0200

    [KNIGHT] add unit tests for forbidden moves

--- tests/test_knight.hh
+++ tests/test_knight.hh
@@ -9,11 +9,25 @@ TEST (Knight, basic_northnortheast)
     EXPECT_EQ(1 << 22, move);
 }
 
+TEST (Knight, northnortheast_out)
+{
+    board::Bitboard knight = 1 << 23;
+    board::Bitboard move = board::Knight::northnortheast(knight);
+    EXPECT_EQ(0, move);
+}
+
 TEST (Knight, basic_northnorthwest)
 {
     board::Bitboard knight = 1 << 9;
     board::Bitboard move = board::Knight::northnorthwest(knight);
     EXPECT_EQ(1 << 24, move);
+}
+
+TEST (Knight, northnorthwest_out)
+{
+    board::Bitboard knight = 1 << 16;
+    board::Bitboard move = board::Knight::northnorthwest(knight);
+    EXPECT_EQ(0, move);
 }
 
 TEST (Knight, basic_northeasteast)
@@ -23,11 +37,25 @@ TEST (Knight, basic_northeasteast)
     EXPECT_EQ(1ULL << 42, move);
 }
 
+TEST (Knight, northeasteast_out)
+{
+    board::Bitboard knight = 1ULL << 54;
+    board::Bitboard move = board::Knight::northeasteast(knight);
+    EXPECT_EQ(0, move);
+}
+
 TEST (Knight, basic_northwestwest)
 {
     board::Bitboard knight = 1 << 27;
     board::Bitboard move = board::Knight::northwestwest(knight);
     EXPECT_EQ(1ULL << 33, move);
+}
+
+TEST (Knight, northwestwest_out)
+{
+    board::Bitboard knight = 1 << 25;
+    board::Bitboard move = board::Knight::northwestwest(knight);
+    EXPECT_EQ(0, move);
 }
 
 TEST (Knight, basic_southsoutheast)
@@ -37,11 +65,25 @@ TEST (Knight, basic_southsoutheast)
     EXPECT_EQ(1ULL << 36, move);
 }
 
+TEST (Knight, southsoutheast_out)
+{
+    board::Bitboard knight = 1ULL << 47;
+    board::Bitboard move = board::Knight::southsoutheast(knight);
+    EXPECT_EQ(0, move);
+}
+
 TEST (Knight, basic_southsouthwest)
 {
     board::Bitboard knight = 1ULL << 60;
     board::Bitboard move = board::Knight::southsouthwest(knight);
     EXPECT_EQ(1ULL << 43, move);
+}
+
+TEST (Knight, southsouthwest_out)
+{
+    board::Bitboard knight = 1ULL << 56;
+    board::Bitboard move = board::Knight::southsouthwest(knight);
+    EXPECT_EQ(0, move);
 }
 
 TEST (Knight, basic_southeasteast)
@@ -51,9 +93,23 @@ TEST (Knight, basic_southeasteast)
     EXPECT_EQ(1 << 7, move);
 }
 
+TEST (Knight, southeasteast_out)
+{
+    board::Bitboard knight = 1ULL << 39;
+    board::Bitboard move = board::Knight::southeasteast(knight);
+    EXPECT_EQ(0, move);
+}
+
 TEST (Knight, basic_southwestwest)
 {
     board::Bitboard knight = 1 << 29;
     board::Bitboard move = board::Knight::southwestwest(knight);
     EXPECT_EQ(1 << 19, move);
+}
+
+TEST (Knight, southwestwest_out)
+{
+    board::Bitboard knight = 1 << 17;
+    board::Bitboard move = board::Knight::southwestwest(knight);
+    EXPECT_EQ(0, move);
 }
