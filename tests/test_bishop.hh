fa348680860bc556aec07296d9f958e006f45770
Guillaume Merle <guillaume.merle@epita.fr>
2020-05-08 12:24:33 +0200

    [BOARD] Bishop: add tests

--- tests/test_bishop.hh
+++ tests/test_bishop.hh
@@ -71,3 +71,39 @@ TEST (Bishop, generate_moves_southeast)
         EXPECT_EQ(pos, move.get_to());
     }
 }
+
+TEST (Bishop, generate_moves_with_chessboard_none_blocking)
+{
+    board::Bitboard bishop = 1;
+    board::Chessboard board;
+    board.set(board::BitboardType::ALLWHITE, 1 << 1);
+
+    std::vector<board::Move> moves;
+    board::Bishop::generate_moves(moves, bishop, board, board::Color::WHITE);
+
+    EXPECT_EQ(7, moves.size());
+}
+
+TEST (Bishop, generate_moves_with_chessboard_one_blocking)
+{
+    board::Bitboard bishop = 1;
+    board::Chessboard board;
+    board.set(board::BitboardType::ALLWHITE, 1 << 27);
+
+    std::vector<board::Move> moves;
+    board::Bishop::generate_moves(moves, bishop, board, board::Color::WHITE);
+
+    EXPECT_EQ(2, moves.size());
+}
+
+TEST (Bishop, generate_moves_with_chessboard_no_move_possible)
+{
+    board::Bitboard bishop = 1 << 27;
+    board::Chessboard board;
+    board.set(board::BitboardType::ALLWHITE, board::FullBB);
+
+    std::vector<board::Move> moves;
+    board::Bishop::generate_moves(moves, bishop, board, board::Color::WHITE);
+
+    EXPECT_EQ(0, moves.size());
+}
