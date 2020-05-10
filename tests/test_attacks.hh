5e8e0f6b4dce169c003b62d722e5c900e254bc06
Alexandre DEISS <alexandre.deiss@epita.fr>
2020-05-10 19:43:06 +0200

    [BOARD] add unit tests for bishop attacks

--- tests/test_attacks.hh
+++ tests/test_attacks.hh
@@ -4,9 +4,9 @@
 
 TEST (Attacks, knight_attacks)
 {
-    auto moves = board::attacks::get_knight_attacks(7);
+    auto moves = board::attacks::get_knight_attacks(56);
 
-    board::Bitboard expected_moves = 1 << 13 | 1 << 22;
+    board::Bitboard expected_moves = 1ULL << 50 | 1ULL << 41;
 
     EXPECT_EQ(moves, expected_moves);
 }
@@ -37,6 +37,28 @@ TEST (Attacks, king_attacks2)
 
     board::Bitboard expected_moves = 1 << 2 | 1 << 3 | 1 << 4 | 1 << 12
                                      | 1 << 10 | 1 << 18 | 1 << 19 | 1 << 20;
+
+    EXPECT_EQ(moves, expected_moves);
+}
+
+TEST (Attacks, bishop_attacks)
+{
+    board::Bitboard occupancy = 1ULL << 36;
+    auto moves = board::attacks::get_bishop_attacks(63, occupancy);
+
+    board::Bitboard expected_moves = 1ULL << 54 | 1ULL << 45 | 1ULL << 36;
+
+    EXPECT_EQ(moves, expected_moves);
+}
+
+TEST (Attacks, bishop_attacks2)
+{
+    board::Bitboard occupancy = 1ULL << 50 | 1 << 9 | 1 << 29 | 1ULL << 54;
+    auto moves = board::attacks::get_bishop_attacks(36, occupancy);
+
+    board::Bitboard expected_moves = 1ULL << 43 | 1ULL << 50 | 1 << 27 | 1 << 18
+                                     | 1 << 9 | 1 << 29 | 1ULL << 45
+                                     | 1ULL << 54;
 
     EXPECT_EQ(moves, expected_moves);
 }
