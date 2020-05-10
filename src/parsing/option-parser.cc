3c8bce872821097f21c6fddd8e2e2b5d13023c66
Valentin Grussenmeyer <valentin.grussenmeyer@epita.fr>
2020-05-10 22:22:04 +0200

    [PERFT] perft function with pseudo move generator implementation

--- src/parsing/option-parser.cc
+++ src/parsing/option-parser.cc
@@ -11,6 +11,26 @@
 
 namespace option_parser
 {
+    /*board::Bitboard perft(int depth)
+    {
+        if (depth == 0)
+            return 1;
+
+        std::vector<board::Move> moves = board::Chessboard::generate_legal_moves();
+        size_t n_moves = moves.size();
+        board::Bitboard nodes = 0;
+        
+        for (size_t i = 0; i < n_moves; ++i)
+        {
+            board::Chessboard::do_move(moves.at(i));
+            if (!board::Chessboard::is_check())
+                nodes += perft(depth - 1);
+            board::Chessboard::undo_move(moves.at(i));
+        }
+        return nodes;
+    }*/
+
+
     namespace po = boost::program_options;
 
     void parse_options(int argc, char** argv)
@@ -21,7 +41,7 @@ namespace option_parser
             ("help,h", "show usage")
             ("pgn", po::value<std::string>(), "path to the PGN game file")
             ("listeners,l", po::value<std::vector<std::string>>()->multitoken(),
-                "list of paths to listener plugins")
+             "list of paths to listener plugins")
             ("perft", po::value<std::string>(), "path to a perft game file")
             ;
 
@@ -55,14 +75,14 @@ namespace option_parser
         if (vm.count("pgn"))
         {
             std::vector<board::PgnMove> pgn_vect = pgn_parser::parse_pgn(
-                                                   vm["pgn"].as<std::string>());
+                    vm["pgn"].as<std::string>());
             //run pgn
         }
 
         if (vm.count("perft"))
         {
             board::PerftObject perft_obj = perft_parser::parse_perft(
-                                                   vm["perft"].as<std::string>());
+                    vm["perft"].as<std::string>());
             //run perft
         }
 
