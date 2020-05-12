0c5648620ba2b76fc7321701f158b7234a661162
Guillaume Merle <guillaume.merle@epita.fr>
2020-05-12 16:57:19 +0200

    [BOARD] Queen: migrate tests

--- tests/test_queen.hh
+++ tests/test_queen.hh
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include "movegen.hh"
 
 #include "queen.hh"
 
@@ -6,67 +7,68 @@
 
 TEST (Queen, generate_moves)
 {
-    board::Bitboard rook = 1 << 28;
-    std::vector<board::Move> moves;
-    board::Queen::generate_moves(moves, rook);
+    board::Chessboard board;
+    board.set(board::WHITE, board::QUEEN, 1 << 28);
+    board.update_all_boards();
+
+    std::vector<board::Move> moves = board::MoveGen(board).get();
 
     EXPECT_EQ(27, moves.size());
 }
 
 TEST (Queen, generate_moves_with_chessboard)
 {
-    board::Bitboard queen = 1;
     board::Chessboard board;
-    board.set(board::WHITE, board::ALL, 1 << 18);
+    board.set(board::WHITE, board::QUEEN, 1);
+    board.update_all_boards();
+    board.set(board::WHITE, board::ALL, 1 | 1 << 18);
 
-    std::vector<board::Move> moves;
-    board::Queen::generate_moves(moves, queen, board);
+    std::vector<board::Move> moves = board::MoveGen(board).get();
 
     EXPECT_EQ(15, moves.size());
 }
 
 TEST (Queen, generate_moves_with_chessboard_multiple_blockers)
 {
-    board::Bitboard queen = 1;
     board::Chessboard board;
-    board::Bitboard blockers = 1 << 18 | 1 << 5 | 1ULL << 32;
+    board.set(board::WHITE, board::QUEEN, 1);
+    board.update_all_boards();
+    board::Bitboard blockers = 1 | 1 << 18 | 1 << 5 | 1ULL << 32;
     board.set(board::WHITE, board::ALL, blockers);
 
-    std::vector<board::Move> moves;
-    board::Queen::generate_moves(moves, queen, board);
+    std::vector<board::Move> moves = board::MoveGen(board).get();
 
     EXPECT_EQ(8, moves.size());
 }
 
 TEST (Queen, generate_moves_with_chessboard_capture)
 {
-    board::Bitboard queen = 1;
     board::Chessboard board;
-
     board::Bitboard pawns = 1 << 8;
-    board.set(board::BLACK, board::PAWN, pawns);
-    board.set(board::BLACK, board::ALL, pawns);
 
-    std::vector<board::Move> moves;
-    board::Queen::generate_moves(moves, queen, board);
+    board.set(board::WHITE, board::QUEEN, 1);
+    board.set(board::BLACK, board::PAWN, pawns);
+    board.update_all_boards();
+
+    std::vector<board::Move> moves = board::MoveGen(board).get();
 
     EXPECT_EQ(15, moves.size());
-    EXPECT_EQ(1 << 8, moves.at(0).get_to());
+    EXPECT_EQ(8, moves.at(0).get_to());
     EXPECT_TRUE(moves.at(0).is_capture());
     EXPECT_EQ(board::PieceType::PAWN, moves.at(0).get_capture());
 }
 
 TEST (Queen, generate_moves_with_chessboard_capture_multiple)
 {
-    board::Bitboard queen = 1;
     board::Chessboard board;
 
+    board.set(board::WHITE, board::QUEEN, 1);
     board::Bitboard pawns = 1 << 1 | 1 << 8 | 1 << 9;
     board.set(board::BLACK, board::PAWN, pawns);
-    board.set(board::BLACK, board::ALL, pawns);
 
-    std::vector<board::Move> moves;
-    board::Queen::generate_moves(moves, queen, board);
+    board.update_all_boards();
+
+    std::vector<board::Move> moves = board::MoveGen(board).get();
 
     EXPECT_EQ(3, moves.size());
     for (auto& move : moves)
