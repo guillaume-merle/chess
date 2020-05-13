43178d690a32041b80c4a777e2c356f17da0c002
Valentin Grussenmeyer <valentin.grussenmeyer@epita.fr>
2020-05-13 14:01:55 +0200

    [EVALUATE] basic negaMax implementation

--- src/chess_engine/ai/search.cc
+++ src/chess_engine/ai/search.cc
@@ -2,20 +2,25 @@
 
 #include <limits>
 
+#include "evaluation.hh"
 #include "search.hh"
 
 namespace board
 {
-    int negaMax(Chessboard board, int depth)
+    int negaMax(Chessboard board, int depth, int color)
     {
         if (depth == 0)
-            return evaluate(board);
+            return evaluate(board) * color;
         int max = std::numeric_limits<int>::max();
-        std::vector<Move> moves = board.generate_legal_moves();
-        for ( all moves)
+        const std::vector<Move> moves = board.generate_legal_moves();
+        for (Move move: moves)
         {
-            score = -negaMax(board, depth - 1);
-            if(score > max)
+            Chessboard new_board = Chessboard(board);
+            newBoard.do_move(move);
+
+            score = negaMax(new_board, depth - 1, color * -1);
+
+            if (score > max)
                 max = score;
         }
         return max;
@@ -24,10 +29,32 @@ namespace board
 
     Move search(Chessboard board)
     {
-        int score = negaMax(board, 1);
-        if (!board.white_turn)
-            score *= -1;
+        int color;
+        int score;
+        int depth = 1;
+        int max = std::numeric_limits<int>::max();
+        const std::vector<Move> moves = board.generate_legal_moves();
+        Move best_move;
 
+        if (board.white_turn)
+            color = 1;
+        else
+            color = -1;
+
+       
+        for (Move move: moves)
+        {
+            Chessboard new_board = Chessboard(board);
+            newBoard.do_move(move);
+
+            score = negaMax(new_board, depth - 1, color);
+
+            if (score > max)
+            {
+                max = score;
+                best_move = move;
+            }
+        }
+        return best_move;
     }
-
 } // namespace board
