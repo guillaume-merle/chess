57cb3f399a1c7d1b1d6b31c385a721f3c25330b6
Marc Demoustier <marc.demoustier@epita.fr>
2020-05-15 11:07:26 +0400

    [BOARD] fix compilation error in move and piece-type

--- src/chess_engine/board/move.hh
+++ src/chess_engine/board/move.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 #include "piece-type.hh"
 #include "bitboard.hh"
--- src/chess_engine/board/piece-type.hh
+++ src/chess_engine/board/piece-type.hh
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <optional>
+#include <iostream>
 
 namespace board
 {
