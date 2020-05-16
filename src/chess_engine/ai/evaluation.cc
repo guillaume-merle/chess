e4675591e9a9918c6010f19b935127b92617b07d
Marc Demoustier <marc.demoustier@epita.fr>
2020-05-16 16:06:40 +0400

    [AI] refacto evaluation function

--- src/chess_engine/ai/evaluation.cc
+++ src/chess_engine/ai/evaluation.cc
@@ -6,42 +6,35 @@ namespace board
     {
         int score = 0;
 
-        if (board.is_checkmate(board.current_color()))
+        Color opposent_color = board.current_color();
+        Color color;
+
+        if (opposent_color == BLACK)
+            color = WHITE;
+        else
+            color = BLACK;
+
+        if (board.is_checkmate(opposent_color))
             score += 900;
-        else if (board.is_check(board.current_color()))
+        else if (board.is_check(opposent_color))
             score += 450;
 
-        score += (4 - popcount(board.get(board.current_color(), QUEEN))) * 90;
-        score += (2 - popcount(board.get(board.current_color(), KNIGHT))) * 50;
-        score += (2 - popcount(board.get(board.current_color(), BISHOP))) * 30;
-        score += (2 - popcount(board.get(board.current_color(), ROOK))) * 50;
-        score += (8 - popcount(board.get(board.current_color(), PAWN))) * 5;
+        score += (4 - popcount(board.get(opposent_color, QUEEN))) * 90;
+        score += (2 - popcount(board.get(opposent_color, KNIGHT))) * 50;
+        score += (2 - popcount(board.get(opposent_color, BISHOP))) * 30;
+        score += (2 - popcount(board.get(opposent_color, ROOK))) * 50;
+        score += (8 - popcount(board.get(opposent_color, PAWN))) * 5;
 
-        if (board.current_color() == BLACK)
-        {
-            if (board.is_checkmate(WHITE))
-                score -= 900;
-            else if (board.is_check(WHITE))
-                score -= 250;
+        if (board.is_checkmate(color))
+            score -= 900;
+        else if (board.is_check(color))
+            score -= 250;
 
-            score += (popcount(board.get(WHITE, QUEEN))) * 90;
-            score += (popcount(board.get(WHITE, KNIGHT))) * 50;
-            score += (popcount(board.get(WHITE, BISHOP))) * 30;
-            score += (popcount(board.get(WHITE, ROOK))) * 50;
-            score += (popcount(board.get(WHITE, PAWN))) * 10;
-        }
-        else
-        {
-            if (board.is_checkmate(BLACK))
-                score -= 900;
-            else if (board.is_check(BLACK))
-                score -= 250;
-            score += (popcount(board.get(BLACK, QUEEN))) * 90;
-            score += (popcount(board.get(BLACK, KNIGHT))) * 50;
-            score += (popcount(board.get(BLACK, BISHOP))) * 30;
-            score += (popcount(board.get(BLACK, ROOK))) * 50;
-            score += (popcount(board.get(BLACK, PAWN))) * 10;
-        }
+        score += (popcount(board.get(color, QUEEN))) * 90;
+        score += (popcount(board.get(color, KNIGHT))) * 50;
+        score += (popcount(board.get(color, BISHOP))) * 30;
+        score += (popcount(board.get(color, ROOK))) * 50;
+        score += (popcount(board.get(color, PAWN))) * 10;
 
         return score;
     }
