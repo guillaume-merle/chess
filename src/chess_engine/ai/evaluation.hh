b5e3ff276bd69c043343ef781920eeb629ce5b15
Marc Demoustier <marc.demoustier@epita.fr>
2020-05-17 23:01:13 +0400

    [AI] add mobility bonus for king in end game

--- src/chess_engine/ai/evaluation.hh
+++ src/chess_engine/ai/evaluation.hh
@@ -70,6 +70,17 @@ namespace ai
         {20, 30, 10, 0, 0, 10, 30, 20}
     };
 
+    const int king_mobility_endgame_bonus[8][8] = {
+        {-50, -40, -30, -20, -20, -30, -40, -50},
+        {-30, -20, -10, 0, 0, -10, -20, -30},
+        {-30, -10, 20, 30, 30, 20, -10, -30},
+        {-30, -10, 30, 40, 40, 30, -10, -30},
+        {-30, -10, 30, 40, 40, 30, -10, -20},
+        {-30, -10, 20, 30, 30, 20, -10, -30},
+        {-30, -30, 0, 0, 0, 0, -30, -30},
+        {-50, -30, -30, -30, -30, -30, -30, -50}
+    };
+
     /**
      * @brief function which evaluate a search, depending the color, the
      * number of pieces and their position on the board.
