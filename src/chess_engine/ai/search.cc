b574c508865f1f2b9e8ba125433f8685f131947e
Guillaume Merle <guillaume.merle@epita.fr>
2020-05-19 18:18:44 +0200

    [AI] Seach: update seach time to 2sec

--- src/chess_engine/ai/search.cc
+++ src/chess_engine/ai/search.cc
@@ -12,7 +12,7 @@ Logger logger;
 namespace ai
 {
     Search::Search()
-        : board_(), us_(board_.current_color()), time_(4)
+        : board_(), us_(board_.current_color()), time_(2)
     {}
 
     Chessboard& Search::get_board()
