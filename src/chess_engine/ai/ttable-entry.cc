28845d670a347f059df68b52385aa10503cfbf1e
Guillaume Merle <guillaume.merle@epita.fr>
2020-05-22 10:14:20 +0200

    [AI] add TTableEntry class

---
+++ src/chess_engine/ai/ttable-entry.cc
@@ -0,0 +1,1 @@
+#include "ttable-entry.hh"
--- src/chess_engine/ai/ttable-entry.hh
+++ src/chess_engine/ai/ttable-entry.hh
@@ -1,17 +1,60 @@
-// #pragma
-//
-// namespace ai
-// {
-    *
-    // @brief Class representing a transposition table entry.
-//
-    // class TtableEntry
-    // {
-    // public:
-        // TtableEntry();
-//
-    // private:
-        // uint64_t
-    // }
-// }
-//
+#pragma once
+
+#include "move.hh"
+
+using namespace board;
+
+namespace ai
+{
+    /**
+     * @brief Enum representing the state of an entry.
+     */
+    enum TTableEntryFlag
+    {
+        EXACT = 0, // Exact score
+        ALPHA, // Score lower than alpha
+        BETA, // score greater than beta
+    };
+
+    /**
+     * @brief Class representing a transposition table entry.
+     *
+     */
+    class TTableEntry
+    {
+    public:
+        /**
+         * @brief Construc a new Transposition Table Entry.
+         */
+        TTableEntry(uint64_t key, int depth, int score, TTableEntryFlag flag,
+                    Move& bestmove);
+
+    private:
+        /**
+         * @brief The zobrist key representing a position of a Chessboard.
+         */
+        uint64_t key_;
+
+        /**
+         * @brief Depth of the search of the entry.
+         */
+        int depth_;
+
+        /**
+         * @brief Score of the search.
+         */
+        int score_;
+
+        /**
+         * @brief Flag representing the search.
+         */
+        TTableEntryFlag flag_;
+
+        /**
+         * @brief Best move of the search.
+         */
+        Move bestmove_;
+    };
+
+} // namespace ai
+
