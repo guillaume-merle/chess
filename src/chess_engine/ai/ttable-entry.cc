668837c8615439ff36bfab8d8288b01527f75bda
Guillaume Merle <guillaume.merle@epita.fr>
2020-05-22 10:16:42 +0200

    [AI] TTableEntry: constructor

--- src/chess_engine/ai/ttable-entry.cc
+++ src/chess_engine/ai/ttable-entry.cc
@@ -1,1 +1,10 @@
 #include "ttable-entry.hh"
+
+namespace ai
+{
+    TTableEntry::TTableEntry(uint64_t key, int depth, int score,
+                             TTableEntryFlag flag, Move& bestmove)
+        : key_(key), depth_(depth), score_(score), flag_(flag)
+          , bestmove_(bestmove)
+    {}
+}
